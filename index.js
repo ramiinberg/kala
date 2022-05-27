597e2205f2032faf8e46d9b774b00314158ae5da
Rami Inberg <rami.inberg@gmail.com>
2022-05-27 12:27:47 +0300

    optimizing header code

---
+++ index.js
@@ -0,0 +1,16 @@
+const appHeader = `
+  <div class="container container-flex">
+    <div class="site-title">
+      <h1>3xPM</h1>
+      <p class="subtitle">Living the simple life</p>
+    </div>
+    <nav>
+      <ul>
+        <li><a class="current-page" href="index.html">Home</a></li>
+        <li><a href="fishTournament.html">Kalakisa</a></li>
+        <li><a href="#">TBA</a></li>
+      </ul>
+    </nav>
+  </div>
+`;
+document.getElementById("app-header").innerHTML = appHeader;
